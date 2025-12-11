baed818ecf3b20ca8e084517b2930d301cfd7955
Abhijit <abhijitmondal012@gmail.com>
2025-12-11 21:37:52 +0530

    add skills and animation

--- JS/main.js
+++ JS/main.js
@@ -18,34 +18,32 @@ document.getElementById("scroll-up").addEventListener("click", (e) => {
   });
 });
 
-
-
-window.addEventListener("scroll", function() {
-const scrollUp = document.getElementById("scroll-up");
-if (window.scrollY > 300) {
-scrollUp.style.display = "block";
-} else {
-scrollUp.style.display = "none";
-}
+window.addEventListener("scroll", function () {
+  const scrollUp = document.getElementById("scroll-up");
+  if (window.scrollY > 300) {
+    scrollUp.style.display = "block";
+  } else {
+    scrollUp.style.display = "none";
+  }
 });
 
-// hamburger menu 
-const toggle = document.querySelector('.navbar-toggle');
-const menu   = document.querySelector('.navbar-menu');
-const bars   = document.querySelectorAll('.bar');
-toggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    bars.forEach(bar => bar.classList.toggle('active'));
-  });
+// hamburger menu
+const toggle = document.querySelector(".navbar-toggle");
+const menu = document.querySelector(".navbar-menu");
+const bars = document.querySelectorAll(".bar");
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  bars.forEach((bar) => bar.classList.toggle("active"));
+});
 
-  // Close menu when clicking any navbar link
-document.querySelectorAll('.navbar-menu a').forEach(link => {
-  link.addEventListener('click', () => {
-    document.querySelector('.navbar-menu').classList.remove('active');
+// Close menu when clicking any navbar link
+document.querySelectorAll(".navbar-menu a").forEach((link) => {
+  link.addEventListener("click", () => {
+    document.querySelector(".navbar-menu").classList.remove("active");
 
     // Remove hamburger animation (X)
-    document.querySelectorAll('.bar').forEach(bar => {
-      bar.classList.remove('active');
+    document.querySelectorAll(".bar").forEach((bar) => {
+      bar.classList.remove("active");
     });
   });
 });
